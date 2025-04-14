1e34cc7e7ad0d1c25c9b53af6ee7481bcd9652d7
Chester Harvey <chesterharvey@gmail.com>
2025-04-14 11:44:13 -0600

    demo 11 updates

--- demos/demo11/app.py
+++ demos/demo11/app.py
@@ -4,17 +4,39 @@ import geopandas as gpd
 
 import plotly.express as px
 from shiny import reactive
-from shiny.express import input, ui
-from shinywidgets import render_widget
+from shiny.express import ui, input, render
+from shinywidgets import render_plotly, render_widget
 from ipyleaflet import GeoJSON, Map, CircleMarker, basemaps
 
-# Retrieve bike data and join with tracts
-tracts_gdf = demo11.load_tracts()
+
+##############
+# Prepare data
+##############
+
+# Load Data
 bikes_gdf, stations_gdf = demo11.load_bike_data()
 bikes_gdf = pd.concat([
     bikes_gdf[['lat','lon','vehicle_type_id','timestamp','geometry']],
     demo11.expand_stations_into_bikes(stations_gdf)])
-bikes_gdf = demo11.attach_tracts_to_bikes(bikes_gdf, tracts_gdf)
+tracts_gdf = demo11.load_tracts()
+eea_df = pd.read_csv('mwcog_eea_2016_2020.csv')
+eea_df = demo11.clean_eea(eea_df)
+nbhds_gdf = demo11.load_nbhds()
+
+# Join equity emphasis area data to tracts
+tracts_gdf = tracts_gdf.merge(eea_df, on='tract_id', how='left')
+
+# Add tract data to nbhds with weighted average eea indices
+# Also limit to nhbds with tract data
+nbhds_gdf = demo11.add_tract_data_to_nbhds(nbhds_gdf, tracts_gdf)
+
+# Attach neighborhoods to bikes
+bikes_gdf = demo11.attach_points_to_points(bikes_gdf, nbhds_gdf[['nbhd','geometry']])
+
+
+######################################
+# Define parameters for ui & selection
+######################################
 
 # Specify fields and codes for selection
 ## Bike Type
@@ -35,8 +57,12 @@ docking_status_codes = {
 bikes_gdf[docking_status_field] = bikes_gdf[docking_status_field].map(docking_status_codes)
 
 # Calculate midpoint for mapping
-mid_lon, mid_lat = demo11.get_gdf_midpoint(tracts_gdf)
+mid_lon, mid_lat = demo11.get_gdf_midpoint(nbhds_gdf)
 
+
+###########
+# Define ui
+###########
 
 with ui.sidebar():
     
@@ -59,27 +85,40 @@ with ui.sidebar():
         # width=None,
     )
 
+@render_plotly
+def bar_chart():
+    # Filter based on inputs
+    filtered_bikes_df = demo11.filter_bikes(
+        bikes_gdf, 
+        {
+            bike_type_field: input.bike_type(),
+            docking_status_field: input.docking_status(),
+        }
+    )
+    # Attach bikes to neighborhoods
+    bikes_per_nbhd_df = demo11.count_bikes_per_nbhd(filtered_bikes_df)
+
+    fig = px.bar(bikes_per_nbhd_df.reset_index(), x="nbhd", y="bikes")
+    return fig
+    
+
 @render_widget  
 def map():
 
-    print(f'Raw bikes: {len(bikes_gdf)}')
-
-    # Filter bikes based on ui criteria (electric/non-electric; docked/undocked)  
-    bikes_to_map_df = bikes_gdf[
-        (bikes_gdf[bike_type_field].isin(input.bike_type()))
-        & (bikes_gdf[docking_status_field].isin(input.docking_status()))
-    ].copy()
-
-    print(f'Filtered bikes: {len(bikes_to_map_df)}')
-
-    # Count bikes per tract for mapping
-    bikes_per_tract_df = demo11.count_bikes_per_tract(bikes_to_map_df)
-    tracts_with_bike_counts_gdf = demo11.attach_counts_to_tract_polygons(bikes_per_tract_df, tracts_gdf)
-    tracts_with_bike_counts_gdf = demo11.polygons_to_points(tracts_with_bike_counts_gdf)
-
-    print(f'Bikes being mapped: {tracts_with_bike_counts_gdf.bikes.sum()}')
-    print(f'Tracts being mapped: {len(tracts_with_bike_counts_gdf)}')
+    # Filter based on inputs
+    filtered_bikes_df = demo11.filter_bikes(
+        bikes_gdf, 
+        {
+            bike_type_field: input.bike_type(),
+            docking_status_field: input.docking_status(),
+        }
+    )
     
+    # Attach bikes to neighborhoods
+    bikes_per_nbhd_df = demo11.count_bikes_per_nbhd(filtered_bikes_df)
+    nbhds_with_bike_counts_gdf = demo11.attach_counts_to_nbhd_points(bikes_per_nbhd_df, nbhds_gdf) 
+
+    # Build map
     m = Map(
         basemap=basemaps.CartoDB.Positron,
         center=(mid_lat, mid_lon), 
@@ -88,20 +127,16 @@ def map():
     )  
 
     multiplier = 0.2
-    tract_counter = 0
-    for tract in tracts_with_bike_counts_gdf.itertuples():
+    for nbhd in nbhds_with_bike_counts_gdf.itertuples():
         circle_marker = CircleMarker()
-        lat = tract.geometry.y
-        lon = tract.geometry.x
-        print(f'lat: {lat}, lon:{lon}')
+        lat = nbhd.geometry.y
+        lon = nbhd.geometry.x
         circle_marker.location = (lat, lon)
-        radius = demo11.int_at_least_one(tract.bikes * multiplier)
+        radius = demo11.int_at_least_one(nbhd.bikes * multiplier)
         circle_marker.radius = radius
         circle_marker.color = "red"
         circle_marker.fill_color = "#ffffff00"
         circle_marker.weight=1
         m.add(circle_marker)
-        tract_counter += 1
-    print(f'Tracts mapped: {tract_counter}')
         
     return m
--- demos/demo11/demo11.py
+++ demos/demo11/demo11.py
@@ -7,31 +7,29 @@ import numpy as np
 
 UTM18 = 26918
 
-def save_json(data, file_name, timestamp=False):
-    """Save data as json file
-    data: json-compatable data structure (nested dicts and lists)
-    file_name: string for file name; DO NOT include file extension (e.g., ".json")
-    """
-    if timestamp:
-        file_name = f'{file_name}_{timestamp}.json'
-    else:
-        file_name = f'{file_name}.json'
-    with open(file_name, 'w') as f:
-        json.dump(data, f, indent=4)
+# def save_json(data, file_name, timestamp=False):
+#     """Save data as json file
+#     data: json-compatable data structure (nested dicts and lists)
+#     file_name: string for file name; DO NOT include file extension (e.g., ".json")
+#     """
+#     if timestamp:
+#         file_name = f'{file_name}_{timestamp}.json'
+#     else:
+#         file_name = f'{file_name}.json'
+#     with open(file_name, 'w') as f:
+#         json.dump(data, f, indent=4)
 
-def get_and_save_cabi_data():
-    """Get current data from the CABI API and save as a timestamped JSON
-    """
-    # Making a "GET" request
-    # response = requests.get('https://gbfs.lyft.com/gbfs/1.1/dca-cabi/en/free_bike_status.json')
+# def get_and_save_cabi_data():
+#     """Get current data from the CABI API and save as a timestamped JSON
+#     """
+#     # Making a "GET" request
+#     # response = requests.get('https://gbfs.lyft.com/gbfs/1.1/dca-cabi/en/free_bike_status.json')
 
-    bike_status = requests.get('https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/free_bike_status.json').json()
+#     bike_status = requests.get('https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/free_bike_status.json').json()
     
-    
-    
-    # Get JSON content
-    data = response.json()
-    return data
+#     # Get JSON content
+#     data = response.json()
+#     return data
 
 def load_tracts():
     """Load tracts from geojson and return as geodataframe
@@ -45,6 +43,13 @@ def load_tracts():
     })
     tracts_gdf =tracts_gdf[['tract_id','pop','geometry']]
     return tracts_gdf
+
+def load_nbhds():
+    """Load neighborhood points from geojson and return as geodataframe
+    """
+    nbhds_gdf = gpd.read_file('dc_neighborhood_points.geojson')
+    nbhds_gdf = nbhds_gdf[['NAME','geometry']].rename(columns={'NAME':'nbhd'})
+    return nbhds_gdf
 
 def load_bike_data():
     """Load bikes from CABI API and return as geodataframe
@@ -91,41 +96,48 @@ def expand_stations_into_bikes(stations_gdf):
                 }) 
     return gpd.GeoDataFrame(pd.DataFrame(station_bikes), geometry='geometry', crs=4326)
 
-def attach_tracts_to_bikes(bikes_gdf, tracts_gdf):
-    """Spatially join tract IDs to bikes
+# def attach_tracts_to_bikes(bikes_gdf, tracts_gdf):
+#     """Spatially join tract IDs to bikes
+#     """
+#     bikes_gdf = gpd.sjoin(bikes_gdf, tracts_gdf[['tract_id','geometry']])
+#     bikes_gdf = bikes_gdf.drop(columns='index_right')
+#     return bikes_gdf
+
+# def attach_nbhds_to_bikes(bikes_gdf, nbhds_gdf):
+#     """Spatially join tract IDs to bikes
+#     """
+#     bikes_gdf = gpd.sjoin_nearest(bikes_gdf.to_crs(UTM18), nbhds_gdf[['nbhd','geometry']].to_crs(UTM18))
+#     bikes_gdf = bikes_gdf.drop(columns='index_right')
+#     return bikes_gdf.to_crs(4326)
+
+def attach_points_to_points(gdf_a, gdf_b):
+    """Spatially join points in one gdf to the closest point in another gdf
     """
-    bikes_gdf = gpd.sjoin(bikes_gdf, tracts_gdf[['tract_id','geometry']])
-    bikes_gdf = bikes_gdf.drop(columns='index_right')
-    return bikes_gdf
+    gdf_a = gpd.sjoin_nearest(gdf_a.to_crs(UTM18), gdf_b.to_crs(UTM18))
+    gdf_a = gdf_a.drop(columns='index_right')
+    return gdf_a.to_crs(4326)
 
-def count_bikes_per_tract(bikes_gdf):
-    return pd.DataFrame(bikes_gdf.groupby('tract_id').size().rename('bikes'))
-    
-    # return bikes_gdf
-    
-    # # Count bikes available in each tract
-    # bikes_per_tract = bikes_gdf.groupby('tract_id').agg({'index':'nunique', 'timestamp':'first'})
-    # bikes_per_tract = bikes_per_tract.rename(columns={'index':'available_bikes'})
-    # return bikes_per_tract
+def count_bikes_per_nbhd(bikes_gdf):
+    return pd.DataFrame(bikes_gdf.groupby('nbhd').size().rename('bikes'))
 
-# Attach counts to tract polygons
-def attach_counts_to_tract_polygons(bikes_per_tract_df, tracts_gdf):
-     return tracts_gdf.merge(bikes_per_tract_df, left_on='tract_id', right_index=True)
+# Attach counts to nbhd points
+def attach_counts_to_nbhd_points(bikes_per_nbhd_df, nbhds_gdf):
+     return nbhds_gdf.merge(bikes_per_nbhd_df, left_on='nbhd', right_index=True)
 
 def proportional_circles_radii(values, multiplier=1):
     """Calculate radii of proportional circles from a column of values given a multiplier
     """
     return np.sqrt(values / 3.14) * multiplier
 
-def replace_polygons_with_proportional_circles(polgyon_gdf, value_col, multiplier=1, calc_crs=UTM18):
-    initial_crs = polgyon_gdf.crs
-    point_gdf = polgyon_gdf.copy()
-    point_gdf = point_gdf.to_crs(calc_crs)
-    point_gdf['geometry'] = point_gdf.centroid
-    point_gdf['geometry'] = point_gdf.buffer(
-        proportional_circles_radii(point_gdf[value_col], multiplier))
-    point_gdf = point_gdf.to_crs(initial_crs)
-    return point_gdf
+# def replace_polygons_with_proportional_circles(polgyon_gdf, value_col, multiplier=1, calc_crs=UTM18):
+#     initial_crs = polgyon_gdf.crs
+#     point_gdf = polgyon_gdf.copy()
+#     point_gdf = point_gdf.to_crs(calc_crs)
+#     point_gdf['geometry'] = point_gdf.centroid
+#     point_gdf['geometry'] = point_gdf.buffer(
+#         proportional_circles_radii(point_gdf[value_col], multiplier))
+#     point_gdf = point_gdf.to_crs(initial_crs)
+#     return point_gdf
 
 def polygons_to_points(polgyon_gdf, calc_crs=UTM18):
     initial_crs = polgyon_gdf.crs
@@ -153,3 +165,59 @@ def int_at_least_one(num):
     if num < 1:
         num = 1
     return num
+
+def filter_bikes(bikes_gdf, filters_dict):
+    bikes_gdf = bikes_gdf.copy()
+    for field, values in filters_dict.items():
+        bikes_gdf = bikes_gdf[bikes_gdf[field].isin(values)]
+    return bikes_gdf
+
+def reformat_eea_census_tract_ids(eea_df):
+    eea_df = eea_df.copy()
+    eea_df['tract_id'] = eea_df['tract_id'].str.split(' ').str[-1].astype(float).apply(lambda x: '{:.2f}'.format(x)).astype(str).str.replace('.','').str.zfill(6)
+    eea_df['tract_id'] = '14000US11001' + eea_df['tract_id']
+    return eea_df
+
+def clean_eea(eea_df):
+    eea_df = eea_df.copy()
+    eea_df = eea_df[eea_df.Jurisdiction == 'District of Columbia']
+    columns = {
+        'Census Tract':'tract_id',
+        'EEA Total Index':'eea_idx',
+        'Concentration Low-Income':'eea_low_income',
+        'Concentration African American or Black':'eea_black',
+        'Concentration Hispanic or Latino':'eea_hisp',
+        'Concentration Asian':'eea_asian',
+    }
+    eea_df = eea_df.rename(columns=columns)
+    eea_df =eea_df[columns.values()]
+    eea_df = reformat_eea_census_tract_ids(eea_df)
+    return eea_df
+
+def w_avg(df, values, weights):
+    """Calculate average across values column weighted by weights column
+    """
+    d = df[values]
+    w = df[weights]
+    return (d * w).sum() / w.sum()
+
+def add_tract_data_to_nbhds(nbhds_gdf, tracts_gdf):
+    nbhds_gdf = nbhds_gdf.copy()
+    tracts_gdf = tracts_gdf.copy()
+    
+    # Attach tract data to the closest neighborhood point
+    tract_points_by_neighborhood = attach_points_to_points(polygons_to_points(tracts_gdf), nbhds_gdf)
+
+    # Calculate sum of population and weighted averages of eea indices per neighborhood
+    nbhds_gdf = nbhds_gdf.set_index('nbhd')
+    nbhds_gdf = pd.concat([nbhds_gdf, tract_points_by_neighborhood.groupby('nbhd').pop.sum()], axis=1)
+    for col in ['eea_idx','eea_low_income','eea_black','eea_hisp','eea_asian']:
+    
+        weighted_average = tract_points_by_neighborhood.groupby('nbhd').apply(w_avg, col, 'pop', include_groups=False)
+        weighted_average.name = col
+        nbhds_gdf = pd.concat([nbhds_gdf, weighted_average], axis=1)
+    
+    # Drop rows with no population (didn't join to tracts)
+    nbhds_gdf = nbhds_gdf[nbhds_gdf['pop'] > 0]
+
+    return nbhds_gdf.reset_index()
